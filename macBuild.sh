ae178ce6144d3ed242a432130811097e7affe796
sodiumts <ernests.malnacs@gmail.com>
2024-07-16 21:00:43 +0300

    Made cmake build in debug mode

--- macBuild.sh
+++ macBuild.sh
@@ -1,5 +1,5 @@
 mkdir -p build
 cd build
-cmake -S ../ -B .
+cmake -S ../ -DCMAKE_BUILD_TYPE=Debug -B .
 make && make Shaders && ./GlorpEngine
 cd ..
--- winBuild.bat
+++ winBuild.bat
@@ -1,5 +1,5 @@
 if not exist build mkdir build
 cd build
-cmake -S ../ -B . -G "MinGW Makefiles"
+cmake -DCMAKE_BUILD_TYPE=Debug -S ../ -B . -G "MinGW Makefiles"
 mingw32-make.exe && mingw32-make.exe Shaders
 cd ..
