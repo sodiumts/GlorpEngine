569392e6002890a1bf1b1d599dfaf8b70dbed2cb
sodiumts <ernests.malnacs@gmail.com>
2025-05-18 00:23:25 +0200

    Changed to just build an app bundle

--- macBuild.sh
+++ macBuild.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
 BUILD_TYPE="Debug"
-BUILD_APP_BUNDLE="OFF"
-
-usage() {
-    echo "Usage: $0 [-r]"
-    echo "  -r    Build in release mode and create an app bundle for macOS"
-    exit 1
-}
-
-while getopts "r" opt; do
-    case ${opt} in
-        r)
-            BUILD_TYPE="Debug"
-            BUILD_APP_BUNDLE="ON"
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
+BUILD_APP_BUNDLE="ON"
 
 echo "Release bundle build = ${BUILD_APP_BUNDLE}"
 echo "Build type = ${BUILD_TYPE}"
@@ -28,7 +10,3 @@ mkdir -p build
 cd build
 cmake -S ../ -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_APP_BUNDLE=${BUILD_APP_BUNDLE} -B .
 make -j && make Shaders
-
-if [ "${BUILD_APP_BUNDLE}" = "OFF" ]; then
-    cd .. && ./build/GlorpEngine
-fi
